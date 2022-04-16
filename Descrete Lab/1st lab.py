833372bd689cbdd092588ce080f659c7db65bdd2
Unknown71-coder <hasansakib047@gmail.com>
2022-04-16 17:58:00 +0600

    format

--- Descrete Lab/1st lab.py
+++ Descrete Lab/1st lab.py
@@ -5,5 +5,5 @@ with open("input.txt", "r", encoding="utf-8") as g:
     res=[(i,j) for i,j in product(S,repeat=2) if i%j==0 or j%i==0]
     res2=[(i,j) for i,j in product(S,repeat=2) if i<=j]
     # printing result
-    print ("The pair list is for a/b : " + str(res))
+print ("The pair list is for a/b : " + str(res))
 print ("The pair list is for a<=b : " + str(res2))
